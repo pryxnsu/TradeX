1a05441dae01e993980e26f19807de881266810b
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-11-04 23:49:01 +0530

    fix: unsubscribe payload to  symbols array

--- types/index.ts
+++ types/index.ts
@@ -11,7 +11,7 @@ export type SocketMessageType =
     | {
           unsubscribe: {
               event: string;
-              symbol: string;
+              symbols: string[];
           };
       }
     | {
